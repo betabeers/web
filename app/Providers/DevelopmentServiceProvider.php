a4d18e67dd2a3524d3ddf31f384dac8e2eede054
Jaime Sares <jaimesares@gmail.com>
2017-01-05 01:22:37 +0100

    installed barryvdh/laravel-ide-helper dependency

---
+++ app/Providers/DevelopmentServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider;
+
+class DevelopmentServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(IdeHelperServiceProvider::class);
+        }
+    }
+}
