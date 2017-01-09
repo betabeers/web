05c43250d8a852af6da2d1a95758544754d46f1e
Jaime Sares <jaimesares@gmail.com>
2017-01-09 04:28:06 +0100

    added country-province relation

--- app/Models/Province.php
+++ app/Models/Province.php
@@ -32,6 +32,11 @@ class Province extends Model
         return $this->hasMany(User::class);
     }
 
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
     public function cities()
     {
         return $this->hasMany(City::class);
