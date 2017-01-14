e4ad4a95c6caf87214b36a28bb176f1039874365
Fernando Carrascosa <fcarrascosa@fcarrascosa.es>
2017-01-14 19:16:23 +0100

    User Login Layout and Styles

---
+++ resources/views/layouts/empty.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="/css/styles.css" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script>
+        window.Laravel = <?php echo json_encode([
+            'csrfToken' => csrf_token(),
+        ]); ?>
+    </script>
+</head>
+<!-- TODO: Dynamicly Add Controller-Action class to Body-->
+<body class="user-login">
+    @yield('content')
+</body>
+</html>
