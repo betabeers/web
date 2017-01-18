3d7c78087ef204cf45e3d576ace94bf26174c79c
Raül Torralba Adsuara <raul.torralba@artansoft.com>
2017-01-19 00:22:10 +0100

    Added variable css class content for views at Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,27 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct()
+    {
+        View::share('cssClass', $this->getCssClassFromRouteName(Route::currentRouteName()));
+    }
+
+    private function getCssClassFromRouteName($routeName)
+    {
+        foreach (explode('.', $routeName) as $key => $segment) {
+            if ($key == 0) {
+                $classArray[0] = $segment;
+            } else {
+                $classArray[$key] = $classArray[$key-1].'-'.$segment;
+            }
+        }
+        return implode(' ', $classArray);
+    }
 }
