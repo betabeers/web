bbab76921ddc92cfa060d94d75e1a3e1409f2c46
fcarrascosa <fcarrascosa@fcarrascosa.es>
2017-01-28 20:20:11 +0100

    Modified recover-password script to load on 'submit' event

--- resources/assets/js/users/recover-password.js
+++ resources/assets/js/users/recover-password.js
@@ -2,40 +2,36 @@
  * Created by fcarrascosa on 28/01/17.
  */
 
-jQuery(document).ready(function() {
+jQuery(document).on('submit', '#modal-password-reset-form', function (e) {
+    e.preventDefault();
 
-    jQuery('#modal-password-reset-form').on('submit', function (e) {
-        e.preventDefault();
+    $this = jQuery(this);
 
-        $this = jQuery(this);
+    $url = jQuery(this).attr('action');
+    $method = jQuery(this).attr('method');
 
-        $url = jQuery(this).attr('action');
-        $method = jQuery(this).attr('method');
+    $params = {};
 
-        $params = {};
+    jQuery(this).find('input').each(function () {
+        $name = jQuery(this).attr('name');
+        $value = jQuery(this).val();
 
-        jQuery(this).find('input').each(function () {
-            $name = jQuery(this).attr('name');
-            $value = jQuery(this).val();
-
-            $params[$name] = $value;
-
-        });
-        console.log($url);
-
-        jQuery.ajax({
-            url: $url,
-            method: $method,
-            data: $params,
-            error: function (response) {
-                console.log(response);
-                $this.find('.messages').empty().append('<div class="alert alert-danger">' + response.responseJSON.message + '</div>').slideDown().delay(1500).slideUp();
-            },
-            success: function (response) {
-                $this.find('.messages').empty().append('<div class="alert alert-success">Te hemos enviado un email</div>').slideDown().delay(1500).slideUp();
-            }
-        })
+        $params[$name] = $value;
 
     });
+    console.log($url);
+
+    jQuery.ajax({
+        url: $url,
+        method: $method,
+        data: $params,
+        error: function (response) {
+            $this.find('.messages').empty().append('<div class="alert alert-danger">' + response.responseJSON.message + '</div>').slideDown().delay(1500).slideUp();
+        },
+        success: function (response) {
+            $this.find('.messages').empty().append('<div class="alert alert-success">Te hemos enviado un email</div>').slideDown().delay(1500).slideUp();
+        }
+    })
 
 });
+
