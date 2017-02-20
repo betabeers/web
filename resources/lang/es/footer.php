1b91fca34599175fff09963719238ae9558c1e0e
fcarrascosa <fcarrascosa@fcarrascosa.es>
2017-02-20 18:54:55 +0100

    Add Footer Translations

---
+++ resources/lang/es/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fcarrascosa
+ * Date: 20/02/17
+ * Time: 18:47
+ */
+
+return [
+
+    'sponsors' => 'Patrocinadores',
+    'participate' => 'Participa',
+    'companies' => 'Empresas',
+    'contact' => 'Contacto',
+    'aboutUs' => 'Sobre Nosotros',
+    'headquarters' => 'Sedes',
+    'codeOfConduct' => 'Código de Conducta',
+    'blog' => 'Blog',
+    'organizeEvent' => 'Organiza un evento',
+    'presentYourStartup' => 'Presenta tu Startup',
+    'lookForDevelopers' => 'Busca Desarrolladores',
+    'support' => 'Apóyanos',
+    'sponsor' => 'Patrocina',
+    'press' => 'Prensa',
+    'allRightsReserved' => 'Todos los derechos reservados'
+];
