8d3c62cf7bd562fd6e1dc05ec52ad7badf2cab96
Raül Torralba Adsuara <raul.torralba@artansoft.com>
2017-03-02 01:10:51 +0100

    Added name, slug and password on twitter sign in and login

--- app/Http/Controllers/Auth/Twitter.php
+++ app/Http/Controllers/Auth/Twitter.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class Twitter extends Controller
 {
@@ -22,16 +23,28 @@ class Twitter extends Controller
             return redirect('auth/twitter');
         }
 
-        $userData = [
-            'twitter_id'    => $user->id,
-            'email'         => $user->email,
-            'name'          => $user->name
-        ];
-
-        $authUser = User::firstOrCreate($userData);
+        $authUser = $this->findOrCreateUser($user);
 
         Auth::login($authUser, true);
 
         return redirect()->route('index');
+    }
+
+    public function findOrCreateUser($user, $provider = null)
+    {
+        $authUser = User::where('twitter_id', $user->id)->first();
+        if ($authUser) {
+            return $authUser;
+        }
+
+        $userData = [
+            'twitter_id'    => $user->id,
+            'email'         => $user->email,
+            'name'          => $user->name,
+            'slug'          => str_slug($user->name),
+            'password'      => bcrypt(time().rand())
+        ];
+
+        return User::create($userData);
     }
 }
